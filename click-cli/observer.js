55c1e21e682800490a4390518af2c4ecddc6e7a6
nirikshan <nirikshanbhusal@gmail.com>
2019-02-23 18:32:58 +0545

    Update observer.js

--- click-cli/observer.js
+++ click-cli/observer.js
@@ -1,72 +1,66 @@
-import Dep from './store.js';
-import { parse } from './render';
-
+import {UpdateData} from './render'
 
 export default class Observer {
 
-    constructor(data, auto , ci) {
+    constructor(data, auto , ci , cn) {
         this.data = data;
-        this.observe(data, auto , ci);
+        this.observe(data, auto , ci , cn);
     }
 
-    observe(data, auto , ci) {
+    observe(data, auto , ci , cn) {
         var self = this;
         if (!data || typeof data !== 'object') {
             return;
         }
         Object.keys(data).forEach(function(key) {
-            self.observeObject(data, key, data[key], auto[key] , ci);
+            self.observeObject(data, key, data[key], auto[key] , ci , cn);
         });
     }
-
-    observeObject(data, key, val, compute , ci) {
-        var dep = new Dep();
+        
+    observeObject(data, key, val, compute , ci , cn) {
         var self = this;
         Object.defineProperty(data, key, {
             enumerable: true,
             configurable: false,
             get: function() {
-                if (Dep.target && !dep.subs.includes(Dep.target)) {
-                    Dep.target['key'] = key;
-                    dep.addSub(Dep.target)
-                }
                 return (void 0 !== compute) ? compute.bind(global.$.S[ci])(val) : val
             },
             set: function(newVal) {
                 if (val === newVal) {
                     return;
                 }
-                val = newVal;
-                if (Array.isArray(newVal)) {
-                    self.observeArray(newVal, dep);
-                } else {
-                    self.observe(newVal, {});
+                if(typeof(newVal) == 'number'){
+                    newVal = newVal.toString();
                 }
-                dep.notify()
+                val = newVal;
+                 UpdateData(val , ci , cn)
+                 if (Array.isArray(newVal)) {
+                     self.observeArray(newVal , ci , cn);
+                 } else {
+                     self.observe(newVal, {} , ci , cn);
+                 }
             },
         });
 
         if (Array.isArray(val)) {
-            self.observeArray(val, dep);
+            self.observeArray(val , ci , cn);
         } else {
-            if(typeof(val) === 'object'){
-                self.observe(val, {});
+            if(typeof(val) === 'object'){  
+                self.observe(val , {} , ci , cn);
             }   
         }
     }
 
-    observeArray(arr, dep) {
+    observeArray(arr , ci , cn) {
         var self = this;
-        arr.__proto__ = self.defineReactiveArray(dep);
-
-        //C(dep.subs, arr)
+        arr.__proto__ = self.defineReactiveArray(ci , cn , arr);
 
         arr.forEach(function(item, z) {
-            self.observe(item, {});
+            self.observe(item, {} , ci , cn);
         });
     }
 
-    defineReactiveArray(dep) {
+    defineReactiveArray(ci , cn , val) {
         var arrayPrototype = Array.prototype;
         var arrayMethods = Object.create(arrayPrototype);
         var self = this;
@@ -97,11 +91,11 @@ export default class Observer {
                     }
 
                     if (method == 'clone') {
-                        self.update(this, args, dep.subs)
+                       
                     } else if (method == 'search') {
-                       var args = self.find(this , args);
+                       
                     } else if (method == 'del') {
-                       self.del(this, args)
+                        self.del(this, args)
                     } else {
                         var result = original.apply(this, args);
                     }
@@ -118,15 +112,11 @@ export default class Observer {
                             break
                     }
 
+                   // console.log(method)
                     if (inserted && inserted.length) {
-                        self.observeArray(inserted, dep)
+                        self.observeArray(inserted , ci , cn)
                     }
-                    // fire the update
-                    dep.notify({
-                        method,
-                        args
-                    });
-
+                    UpdateData(val , ci , cn)
                     return result
                 },
                 enumerable: true,
@@ -138,63 +128,4 @@ export default class Observer {
         return arrayMethods;
     }
 
-    update(a, b, c) { //clone tail
-        if (Array.isArray(a) && typeof(b) == 'object') {
-            var b = b[b.length - 1],
-                F = false;
-            for (var i = 0; i < a.length; i++) {
-                if (a[i]['id'] == b['id']) {
-                    F = true;
-                    break
-                }
-            }
-            if (F) {
-                var xZ = Object.keys(a[i]);
-                for (var j = 0; j < xZ.length; j++) {
-                    a[i][xZ[j]] = b[xZ[j]];
-                }
-            } else {
-                a.push(b)
-            }
-        }
-    }
-
-    del(that, args){
-        for (var j = 0; j < that.length; j++) {
-            if (that[j]['id'] == args[0]) {
-                that.splice(j, 1);
-                break;
-            }
-        }
-    }
-
-    find(sel , args){
-        var aF = [];
-        sel.forEach(function(arr, gg) {
-            var Yi = parse(args[0] , arr) || '';
-            if (args[2]) {
-                var Yi = Yi.toString();
-            } else {
-                var Yi = Yi.toString().toLowerCase();
-                args[1] = args[1].toLowerCase();
-            }
-
-            if (typeof(Yi) == 'string' || typeof(Yi) == 'number') {
-                if (Yi.indexOf(args[1].toString()) > -1) {
-                    aF.push(arr)
-                }
-            }
-        })
-
-        if (aF.length <= 0) {
-            if (args[3] !== undefined && typeof(args[3]) == 'function') {
-                args[3](false, args[1], null, sel.length)
-            }
-        } else {
-            if (args[3] !== undefined && typeof(args[3]) == 'function') {
-                args[3](true, args[1], aF, sel.length);
-            }
-        }
-        return(aF);
-    }
 }
